6bcc033cab8432a9fa5f5a67a54b2aa8fd3ad84d
divine016 <koutidivine@gmail.com>
2024-02-05 05:45:27 +0100

    added else

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -11,19 +11,19 @@ def min_coins(coins, total):
 
     if total <= 0:
         return 0
-    
-    array = [float('inf')] * (total + 1)
-
-    # o is the minimum number of coins needed to make a change
-    array[0] = 0
-
-    for coin in coins:
-        
-        for amount in range(coin, total + 1):
-            array[amount] = min(array[amount], array[amount - coin] + 1)
-
-    # If array[total] is still float('inf'), no combination of coins is possible
-    if array[total] == float('inf'):
-        return -1
     else:
-        return array[total]
+        array = [float('inf')] * (total + 1)
+
+        # o is the minimum number of coins needed to make a change
+        array[0] = 0
+
+        for coin in coins:
+            
+            for amount in range(coin, total + 1):
+                array[amount] = min(array[amount], array[amount - coin] + 1)
+
+        # If array[total] is still float('inf'), no combination of coins is possible
+        if array[total] == float('inf'):
+            return -1
+        else:
+            return array[total]
